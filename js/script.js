1ceb04c82251e9226a457dc899a843bbc02a8bcf
Pawel Paluch <info@pixelvision.pl>
2023-09-10 13:26:30 +0200

    add conts to script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+{
+    const onChangeBackgroundClick = () => {
+        const bodyElement = document.querySelector(".js-body");
+        const bodyBackgroundElement = document.querySelector(".js-bodyBackground")
 
-const buttonBackgroundElement = document.querySelector(".js-buttonBackground");
-const bodyBackgroundElement = document.querySelector(".js-bodyBackground");
-const bodyElement = document.querySelector(".js-body");
+        bodyElement.classList.toggle("body--changeBackground");
+        bodyBackgroundElement.innerText = bodyElement.classList.contains("body--changeBackground") ? "kosmiczny" : "czarny";
 
-buttonBackgroundElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("body--changeBackground");
-    bodyBackgroundElement.innerText = bodyElement.classList.contains("body--changeBackground") ? "kosmiczny" : "czarny";
-});
+    };
 
+    const init = () => {
+        const buttonBackgroundElement = document.querySelector(".js-buttonBackground");
+        buttonBackgroundElement.addEventListener("click", onChangeBackgroundClick);
+    };
 
+    init();
+}
